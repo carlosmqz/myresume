bbcbc95b003c1c2fb79dbe289796d057a715cce7
Carlos Marquez <carlosmqz@gmail.com>
2019-05-29 19:25:14 -0500

    added python.config file for static content management

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
-def hello():
+@application.route('/')
+def index():
     return render_template("index.html")
 
-@app.route('/skills')
+@application.route('/skills')
 def skills():
     return render_template("skills.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run()
