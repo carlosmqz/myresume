ddd4221705db8adb27eeff55f0b5c5cb29d393d7
Carlos Marquez <carlosmqz@gmail.com>
2019-05-29 21:06:43 -0500

    new deployment

--- application.py
+++ application.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 application = Flask(__name__)
 
 @application.route('/')
+@application.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template("/index.html")
 
 @application.route('/skills')
 def skills():
     return render_template("skills.html")
 
 if __name__ == '__main__':
-    application.run()
+    application.run(host='0.0.0.0')
